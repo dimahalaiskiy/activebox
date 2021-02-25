aac2a80ff8a56e017054e1c2c2ff24db6523e898
Dmitriy <dgalaiskiy@icloud.com>
2021-02-25 11:48:27 +0200

    header_jquery

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH;
+    let scrollPos = $(window).scrollTop()
+
+    $(window).on("scroll load resize", function() {
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if( scrollPos >introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+        
+
+    })
+
+    console.log(scrollPos);
+
+    header.addClass("fixed");
+    
+
+});
